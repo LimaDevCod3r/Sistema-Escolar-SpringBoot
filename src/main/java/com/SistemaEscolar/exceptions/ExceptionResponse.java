30cd443d882e6bd372d26f373ef278ec23ed7764
Diego Lima <diegolima20100@gmail.com>
2025-05-08 15:15:56 -0300

    refactor: transforma a propriedade errors de ExceptionResponse em genérica

--- src/main/java/com/SistemaEscolar/exceptions/ExceptionResponse.java
+++ src/main/java/com/SistemaEscolar/exceptions/ExceptionResponse.java
@@ -4,11 +4,12 @@ package com.SistemaEscolar.exceptions;
 import java.time.Instant;
 import java.util.List;
 
-public record ExceptionResponse(
+public record ExceptionResponse<T>(
         Instant timestamp,
         int status,
         String error,
         String message,
         String path,
-        List<FieldValidationError> errors
-) {}
+        T errors
+) {
+}
